f395567d70a69c7724c1a1df658e130f14102337
lex <lex@lex>
2024-01-05 19:19:31 +0300

    initial commit

---
+++ src/main/java/example/car/carbackend/CarBackendApplication.java
@@ -0,0 +1,13 @@
+package example.car.carbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarBackendApplication.class, args);
+	}
+
+}
